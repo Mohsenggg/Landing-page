64d2b050b4a834f3224f410dda96e2abf03bf1d5
Mohsen <mohse.gamal@gmail.com>
2023-02-01 23:10:15 +0200

    Adding Section 4 With JS

--- js/app.js
+++ js/app.js
@@ -40,6 +40,19 @@
 
 // build the nav
 
+// select the first (main) heading of the page
+const mainHead = document.querySelector("main");
+    // add the <section> element as the last child element of the main heading
+    const content = `<section id="section4" data-nav="Section 4">
+    <div class="landing__container">
+    <h2>Section 4</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+
+    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+    </div>
+    </section>`;
+    // Add section 4 as a last child of main element 
+    mainHead.insertAdjacentHTML("beforeend", content);
 
 // first step >> get the location nav list by id.
 const navList = document.getElementById("navbar__list");
@@ -66,7 +79,6 @@ sectionHeads.forEach(function(item){
     // add li elements to html body as a child of nav element.
     navList.appendChild(navLi);
 });
-
 
 // Select all the 4 sections of the page 
 const sections = document.querySelectorAll('section')
